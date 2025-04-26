cb3a86b13324b3d6e23f8788cc702feda9cfb64a
Muhammed Turgut <muhammedtur20@gmail.com>
2025-04-26 13:06:07 +0300

    Androdi Bootcamp birinci proje

---
+++ app/src/main/java/com/vrstressroom/androidbootcamp/AndroidBootCamp/FirstHomework.kt
@@ -0,0 +1,100 @@
+package com.vrstressroom.androidbootcamp.AndroidBootCamp
+
+
+fun Main(){
+    var sehir: String = "Ankara"
+    var ulke: String = "Türkiye"
+    var telefon: String = "0501 101 00 00"
+    var postaKodu: Int = 63000
+    var meslek: String = "Bilgisayar Mühendisi"
+    var stokMiktarı: Double = 100000.0 //100 Bin
+    val musteriAdı: String = "Ahmed Koç"
+    var bakiye: Double = 1000.0
+    var dogumTarihi: String = "14.09.2005"
+    var maas: Double = 10000.0
+    var medeniDurum: Boolean=false
+    var urunYorumu: String = "Bu urun mukemel"
+    var odemeTarihi: String = "15.05.2025"
+    var odeme:String = "Nakit"
+    var siparisAdedi: Int = 10000
+    val arabaModeli: String = "Full asiret paket passat"
+    val kitapAdi: String = "Clean Code"
+    val yayinlanmaTarihi: String = "12.03.1978"
+    var indirmMiktarı: Int = 1000
+    var odaSayisi: Int = 20
+    var enlem: Double = 334353.0
+    var boylam: Double = 34454545.0
+    val urunAdi: String = "Kalemlik"
+    var yemekFiyati: Int = 100
+    var marka: String = "Elon Oglu INŞ. LTD."
+    var muzikAdi: String = "Onlar havlar"
+    var videoSuresi: Int = 10
+    var urunPuani: Int = 5
+    var resimAdi: String = "Monalisa"
+    var dosyaFormatı: String = ".zip"
+    var renk: String = "Kırmızı"
+    var renkKodu: String = "#FF54644"
+    var telefonModeli:String = "Iphone 45"
+    var ekranBoyutu:String = "11.5 inc"
+    var agirlik: String = "0.500 KG"
+    var ulusalGun: String = "Ramazan Bayramı"
+    var tatilGunu: String = "Haftasonu"
+    var rezervasyonTarihi:String= "11.07.2025"
+    var sokakAdi:String = "Gul sokak"
+    var otobusHatti:String ="90"
+    var kalanDakika: Int = 12
+    var takipKodu:Float = 312434.0f
+    val kuponSuresi:Int = 10
+    val kuponKodu: String = "KASI10NISAN"
+    var faturaAdresi: String = "Trabzon Trabzonlular Ilcesi Trabzonu Sevenler Mahhlesi 61.Sokak Trabzonlular APT."
+
+
+        print(
+            "sehir: ${sehir}\n" +
+                    "ulke: ${ulke}\n" +
+                    "telefon: ${telefon}\n" +
+                    "postaKodu: ${postaKodu}\n" +
+                    "meslek: ${meslek}\n" +
+                    "stokMiktarı: ${stokMiktarı}\n" +
+                    "musteriAdı: ${musteriAdı}\n" +
+                    "bakiye: ${bakiye}\n" +
+                    "dogumTarihi: ${dogumTarihi}\n" +
+                    "maas: ${maas}\n" +
+                    "medeniDurum: ${medeniDurum}\n" +
+                    "urunYorumu: ${urunYorumu}\n" +
+                    "odemeTarihi: ${odemeTarihi}\n" +
+                    "odeme: ${odeme}\n" +
+                    "siparisAdedi: ${siparisAdedi}\n" +
+                    "arabaModeli: ${arabaModeli}\n" +
+                    "kitapAdi: ${kitapAdi}\n" +
+                    "yayinlanmaTarihi: ${yayinlanmaTarihi}\n" +
+                    "indirmMiktarı: ${indirmMiktarı}\n" +
+                    "odaSayisi: ${odaSayisi}\n" +
+                    "enlem: ${enlem}\n" +
+                    "boylam: ${boylam}\n" +
+                    "urunAdi: ${urunAdi}\n" +
+                    "yemekFiyati: ${yemekFiyati}\n" +
+                    "marka: ${marka}\n" +
+                    "muzikAdi: ${muzikAdi}\n" +
+                    "videoSuresi: ${videoSuresi}\n" +
+                    "urunPuani: ${urunPuani}\n" +
+                    "resimAdi: ${resimAdi}\n" +
+                    "dosyaFormatı: ${dosyaFormatı}\n" +
+                    "renk: ${renk}\n" +
+                    "renkKodu: ${renkKodu}\n" +
+                    "telefonModeli: ${telefonModeli}\n" +
+                    "ekranBoyutu: ${ekranBoyutu}\n" +
+                    "agirlik: ${agirlik}\n" +
+                    "ulusalGun: ${ulusalGun}\n" +
+                    "tatilGunu: ${tatilGunu}\n" +
+                    "rezervasyonTarihi: ${rezervasyonTarihi}\n" +
+                    "sokakAdi: ${sokakAdi}\n" +
+                    "otobusHatti: ${otobusHatti}\n" +
+                    "kalanDakika: ${kalanDakika}\n" +
+                    "takipKodu: ${takipKodu}\n" +
+                    "kuponSuresi: ${kuponSuresi}\n" +
+                    "kuponKodu: ${kuponKodu}\n" +
+                    "faturaAdresi: ${faturaAdresi}\n"
+        )
+    }
+
